79adf2c9a0ebe7c51dff69218a0b6ea7d3cb48f9
Alexandre Trufanow <git@minago.org>
2026-03-10 01:25:35 +0100

    update for zellij + better jj

--- home/dot_config/nvim/lua/plugins/vcs.lua
+++ home/dot_config/nvim/lua/plugins/vcs.lua
@@ -1,15 +1,61 @@
 return {
-  { "yannvanhalewyn/jujutsu.nvim", dependencies = { "clabby/difftastic.nvim" } },
   {
-    "clabby/difftastic.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "folke/snacks.nvim",
-    },
+    "nicolasgb/jj.nvim",
+    dependencies = { "folke/snacks.nvim", "esmuellert/codediff.nvim" },
     opts = {
-      download = true, -- prebuilt binary
-      snacks_picker = {
-        enabled = true,
+      diff = {
+        backend = "codediff",
+      },
+    },
+    keys = {
+      {
+        "<leader>jl",
+        function()
+          require("jj.cmd").log()
+        end,
+        desc = "jj: Log",
+      },
+      {
+        "<leader>js",
+        function()
+          require("jj.cmd").status()
+        end,
+        desc = "jj: Status",
+      },
+      {
+        "<leader>jm",
+        function()
+          require("jj.cmd").describe()
+        end,
+        desc = "jj: Describe",
+      },
+      {
+        "<leader>jd",
+        function()
+          require("jj.cmd").diff()
+        end,
+        desc = "jj: Diff",
+      },
+      {
+        "<leader>jc",
+        function()
+          require("jj.cmd").commit()
+        end,
+        desc = "jj: Commit (checkpoint)",
+      },
+      {
+        "<leader>jn",
+        function()
+          require("jj.cmd").new()
+        end,
+        desc = "jj: New change",
+      },
+      {
+        "<leader>jp",
+        function()
+          require("jj.picker").status()
+        end,
+        desc = "jj: Pick status",
       },
     },
   },
