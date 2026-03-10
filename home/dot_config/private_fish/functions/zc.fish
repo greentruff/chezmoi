37c501f2f2a2c7d3fdc820652a811356f52185b7
Alexandre Trufanow <git@minago.org>
2026-03-10 09:34:07 +0100

    fix config

--- home/dot_config/private_fish/functions/zc.fish
+++ home/dot_config/private_fish/functions/zc.fish
@@ -1,3 +1,3 @@
 function zc --description "Start a zellij session with the code layout"
-    zellij --session (basename $PWD) --layout code
+    zellij --layout code attach --create (basename $PWD)
 end
--- home/dot_config/private_fish/functions/zz.fish
+++ home/dot_config/private_fish/functions/zz.fish
@@ -1,8 +1,8 @@
 function zz --description "Attach to a zellij session interactively or by name"
     if set -q argv[1]
-        zellij attach $argv[1]
+        zellij attach --create $argv[1]
     else
-        zellij list-sessions -s | fzf --print-query | tail -1 | read -l session
+        zellij list-sessions -s | fzf | read -l session
         and zellij attach $session
     end
 end
